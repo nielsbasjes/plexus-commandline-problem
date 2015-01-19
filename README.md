4c746f9041d5c0279790293e3c6f41801daf90d9
Niels Basjes <niels@basjes.nl>
2015-01-20 00:29:26 +0100

    Rewrite to reproduce the problem as a unit test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # plexus-commandline-problem
 Reproducing a problem in Plexus
+
+Just do
+    mvn test
+to build and run the tests
+
+This code is published under the Apache 2.0 License.
+So it is fine to copy this unit test into the plexus code base.
--- src/test/java/nl/basjes/bugreports/TestRunningCommandline.java
+++ src/test/java/nl/basjes/bugreports/TestRunningCommandline.java
@@ -4,25 +4,37 @@ import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.CommandLineUtils;
 import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.StreamConsumer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 
-public class RunExpect {
+public class TestRunningCommandline {
 
-    public static void main(String[] args) throws IOException {
-        RunExpect re = new RunExpect();
+    private static final Logger LOG = LoggerFactory.getLogger(TestRunningCommandline.class);
+    private static final long RUNS = 10000;
+
+    @Test
+    public void runExpectManyTimes() throws IOException {
+        TestRunningCommandline re = new TestRunningCommandline();
 
         long runs = 0;
         long ok = 0;
 
-        for (int i = 0; i < 1000; i++) {
+        for (long i = 0; i < RUNS; i++) {
             runs++;
             if (re.runExpect()) {
                 ok++;
             }
+            if (runs % 100 == 0) {
+                LOG.info("Result: {}/{} = {}%", ok, runs, 100 * ((double)ok/(double)runs));
+            }
         }
+        LOG.info("Result: {}/{} = {}%", ok, runs, 100 * ((double)ok/(double)runs));
+        Assert.assertEquals("All runs MUST be ok",runs,ok);
 
-        System.out.println("Result: " + ok + "/" + runs);
     }
 
     public boolean runExpect()
@@ -33,9 +45,6 @@ public class RunExpect {
         cl.createArg().setValue("-c");
         cl.createArg().setValue("send_user \"ONE \"");
 
-//        cl.createArg().setValue( "-c" );
-//        cl.createArg().setValue( "sleep 1" );
-
         cl.createArg().setValue("-");
 
         final LogStreamConsumer stdout = new LogStreamConsumer();
@@ -43,12 +52,7 @@ public class RunExpect {
 
         try {
             final InputStream is = new ByteArrayInputStream(writeExpectScriptFile());
-
             int result = CommandLineUtils.executeCommandLine(cl, is, stdout, stderr);
-            if (result != 0) {
-                throw new IllegalStateException("Expect execution returned: \'" + result + "\' executing \'"
-                        + cl.toString() + "\'");
-            }
         } catch (CommandLineException e) {
             final IllegalStateException ise = new IllegalStateException("Unable to run expect");
             ise.initCause(e);
@@ -62,7 +66,7 @@ public class RunExpect {
      * Writes the expect "script".
      *
      * @return The expect script as a {@code byte[]}.
-     * @throws IOException
+     * @throws java.io.IOException
      */
     private byte[] writeExpectScriptFile()
             throws IOException {
